9ae3efdee3959d77152244cf92820080331e8bfd
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2023-12-18 18:10:42 -0500

    Trying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Card from './Card';
+import TaskCard from './TaskCard';
 
 function App() {
+  const [tasks, setTasks] = useState(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+  const [newTask, setNewTask] = useState('');
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+  // Save tasks to local storage whenever tasks change
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = (taskToDelete) => {
+    const updatedTasks = tasks.filter((task) => task !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        onAddTask={handleAddTask}
+      />
+      <h1 className='taskToCompleteheader'>Task's to complete</h1>
+      <div className="tasks-container">
+        {tasks.map((task, index) => (
+          <TaskCard key={index} task={task} onDeleteTask={handleDeleteTask} />
+        ))}
+      </div>
     </div>
   );
 }
