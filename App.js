869392a17c7009dbf1327737b4240e978f9b9759
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2023-12-20 16:48:35 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -2,49 +2,61 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './Card';
 import TaskCard from './TaskCard';
+import './taskCard.css';
+import giphyGif from './giphy.gif';
 
-function App() {
+export default function App() {
+  const [newTask, setNewTask] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedImportance, setSelectedImportance] = useState('');
+
+
   const [tasks, setTasks] = useState(() => {
     const storedTasks = localStorage.getItem('tasks');
     return storedTasks ? JSON.parse(storedTasks) : [];
   });
-  const [newTask, setNewTask] = useState('');
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  useEffect(() => {
-    const storedTasks = localStorage.getItem('tasks');
-    if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
-    }
-  }, []);
 
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
+      const currentDate = selectedDate || new Date().toISOString().split('T')[0];
+      setTasks([...tasks, { task: newTask, date: currentDate, importance: selectedImportance }]);
       setNewTask('');
+      setSelectedImportance('');
     }
   };
 
   const handleDeleteTask = (taskToDelete) => {
-    const updatedTasks = tasks.filter((task) => task !== taskToDelete);
+    const updatedTasks = tasks.filter((task) => task.task !== taskToDelete.task);
     setTasks(updatedTasks);
   };
+  const handleSelectImportance = (importanceValue) => {
+    // Do something with the selected importance, if needed
+    setSelectedImportance(importanceValue);
+  };
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div>
       <Card
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        onAddTask={handleAddTask}
-      />
+  value={newTask}
+  onChange={(e) => setNewTask(e.target.value)}
+  onAddTask={handleAddTask}
+  onSelectDate={setSelectedDate}
+  onSelectImportance={handleSelectImportance}  // Pass the function here
+/>
       <h1 className='taskToCompleteheader'>Task's to complete</h1>
       <div className="tasks-container">
-        {tasks.map((task, index) => (
-          <TaskCard key={index} task={task} onDeleteTask={handleDeleteTask} />
+        {tasks.map((taskData, index) => (
+          <TaskCard 
+            key={index} 
+            taskData={taskData} 
+            onDeleteTask={handleDeleteTask}
+            onSelectImportance={setSelectedImportance}
+          />
         ))}
       </div>
     </div>
   );
-}
-
-export default App;
+};
